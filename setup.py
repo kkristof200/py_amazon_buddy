d9b8409d3fb69143dd70833e58508f033193bb63
Kristof <kristof.kovacs@telenav.com>
2020-07-03 00:52:50 +0300

    new version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ else:
 
 setuptools.setup(
     name="amazon_buddy",
-    version="0.0.2",
+    version="0.0.3",
     author="Kristof",
     description="python wrapper for the amazon_buddy npm package",
     long_description=long_description,
