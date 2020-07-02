56605ef19947abca6b75a6bad24c92991edc778c
Kristof <kristof.kovacs@telenav.com>
2020-07-02 23:18:12 +0300

    no message

--- README.md
+++ README.md
@@ -14,3 +14,12 @@ pip install amazon_buddy
 # or
 pip3 install amazon_buddy
 ~~~~
+
+## Usage
+
+~~~~python
+from amazon_buddy import AmazonBuddy
+
+print(AmazonBuddy.get_product_details('macbook'))
+print(AmazonBuddy.get_reviews('B01GW3H3U8', min_rating=4))
+~~~~
--- demo.py
+++ demo.py
@@ -1,6 +1,4 @@
-from kcu import kjson
-
 from amazon_buddy import AmazonBuddy
 
-# kjson.print(AmazonBuddy.get_product_details('B01GW3H3U8'))
-kjson.print(AmazonBuddy.get_reviews('B01GW3H3U8', min_rating=4))
+print(AmazonBuddy.get_product_details('macbook'))
+print(AmazonBuddy.get_reviews('B01GW3H3U8', min_rating=4))
